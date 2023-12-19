67157aeb0ba422559fa277422c7f87a67afe50e4
23A91A6192 <147469851+23A91A6192@users.noreply.github.com>
2023-12-19 10:56:21 +0530

    #include<stdio.h> int main() {     int i,n;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);
        }
        int a,count=0;
        scanf("%d",&a);
        for(i=0;i<n;i++)
       
        {
            if(a==arr[i])
            count++;
        }
        {
            printf("%d",count);
        }
    }

---
+++ Count_of_given_element_in_an_array.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    
+    }
+    int a,count=0;
+    scanf("%d",&a);
+    for(i=0;i<n;i++)
+   
+    {
+        if(a==arr[i])
+        count++;
+    }
+    {
+        printf("%d",count);
+    }
+}
